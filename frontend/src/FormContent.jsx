43abd0c328c62829e766c98903df5083e1137402
Hiren Parmar <hirenparmar_r@yahoo.com>
2025-05-27 18:32:36 -0700

    Added Form Validations, and Ready for Backend call

--- frontend/src/FormContent.jsx
+++ frontend/src/FormContent.jsx
@@ -6,12 +6,28 @@ export default function FormContent() {
     const [romanNumber, setRomanNumber] = useState('I');
     const [number, setNumber] = useState(1);
 
+    const isValid = () => {
+        return number >= 1 && number <= 3999;
+    }
+
+    const convertToRoman = () => {
+        //Fetch roman number from Backend and
+        //setRomanNumer()
+    }
+
     return (
-        <Flex>
-            <Form>
-                <TextField value={number}></TextField>
-                <Button> Convert to Roman Numeral </Button>
-                <Text> Roman Numeral : {romanNumber} </Text>
+        <Flex direction="column" alignItems="center" justifyContent="center" height="85vh" gap="size-300">
+            <Form maxWidth="size-3600">
+                <TextField value={number}
+                    validationState={isValid() ? "valid" : "invalid"}
+                    errorMessage="Only 'numbers' between 1 to 3999 allowed"
+                    inputMode="numeric" 
+                    onChange={(val) => {setNumber(val)}} 
+                    label="Enter a Number" 
+                    maxLength="4"
+                />
+                <Button onClick={convertToRoman} isDisabled={!isValid()}> Convert to Roman Numeral </Button>
+                <Text alignSelf="center" UNSAFE_style={{ textAlign: "center", fontSize: "20px" }}> Roman Numeral : {romanNumber} </Text>
             </Form>
         </Flex>
     );
